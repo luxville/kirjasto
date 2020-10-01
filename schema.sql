54d855e9105be5b4e953516256771a14caa969f5
Ville Luostarinen <ville.luostarinen@helsinki.fi>
2020-10-01 15:19:34 +0300

    Update SQL schema

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE authors (
     dead INTEGER,
     description TEXT);
 
-CREATE TABLE material (
+CREATE TABLE librarymaterial (
     id SERIAL PRIMARY KEY,
     name TEXT,
     author_id INTEGER,
@@ -15,20 +15,20 @@ CREATE TABLE material (
     type_id INTEGER,
     age INTEGER);
 
-CREATE TABLE types (
+CREATE TABLE materialtypes (
     id SERIAL PRIMARY KEY,
     name TEXT);
 
-INSERT INTO types (name) VALUES ('Kirja');
-INSERT INTO types (name) VALUES ('Lehti');
-INSERT INTO types (name) VALUES ('Blue-ray-levy');
-INSERT INTO types (name) VALUES ('Muu');
-INSERT INTO types (name) VALUES ('DVD-levy');
-INSERT INTO types (name) VALUES ('CD-levy');
+INSERT INTO materialtypes (name) VALUES ('Kirja');
+INSERT INTO materialtypes (name) VALUES ('Lehti');
+INSERT INTO materialtypes (name) VALUES ('Blue-ray-levy');
+INSERT INTO materialtypes (name) VALUES ('Muu');
+INSERT INTO materialtypes (name) VALUES ('DVD-levy');
+INSERT INTO materialtypes (name) VALUES ('CD-levy');
 
 CREATE TABLE accounts (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    username TEXT,
+    username TEXT UNIQUE,
     password TEXT,
     age INTEGER);
