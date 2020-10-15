16863333dfbd8749a438f1733f58ebf26f9db77e
Ville Luostarinen <ville.luostarinen@helsinki.fi>
2020-10-15 19:19:17 +0300

    Add more loan and return functions

--- loans.py
+++ loans.py
@@ -23,6 +23,8 @@ def loan(account_id, material_id):
     loaned = find_loan_count(account_id, material_id)
     if loaned != 0:
         return False
+    if number_of_free(material_id) == 0:
+        return False
     sql = "INSERT INTO loans (account_id, material_id, returned) VALUES (:account_id, " \
         ":material_id, False)"
     db.session.execute(sql, {"account_id":account_id, "material_id":material_id})
@@ -44,12 +46,20 @@ def find_loan_id(account_id, material_id):
     return loan_id
 
 def number_of_free(material_id):
-    sql = "SELECT COUNT (material_id) FROM loans WHERE material_id=:material_id"
+    sql = "SELECT COUNT (*) FROM loans WHERE material_id=:material_id " \
+        "AND returned=False"
+    result = db.session.execute(sql, {"material_id":material_id})
+    number_of_loaned = result.fetchone()[0]
+    number_of_material = librarymaterial.get_work(material_id)[4]
+    free = number_of_material - number_of_loaned
+    return free
+
+def number_of_loans(material_id):
+    sql = "SELECT COUNT (*) FROM loans WHERE material_id=:material_id"
     result = db.session.execute(sql, {"material_id":material_id})
     number_of_loans = result.fetchone()[0]
-    number_of_material = librarymaterial.get_work(material_id)[4]
-    free = number_of_material - number_of_loans
-    return free
+    print(number_of_loans, "hep")
+    return number_of_loans
 
 def get_loans(account_id):
     sql = "SELECT lo.material_id, li.name, li.type_id, m.name FROM loans lo JOIN " \
@@ -58,6 +68,16 @@ def get_loans(account_id):
     result = db.session.execute(sql, {"account_id":account_id})
     l_list = result.fetchall()
     return l_list
+
+def get_loan_history():
+    #"REPLACE(REPLACE(returned, False, 'Lainassa'), True, 'Palautettu') AS returned "\
+    sql = "SELECT li.name, auth.surname, auth.first_name, m.name, acc.name, lo.returned " \
+        "FROM loans lo JOIN librarymaterial li ON lo.material_id=li.id JOIN materialtypes " \
+        "m ON li.type_id=m.id JOIN authors auth ON li.author_id=auth.id JOIN accounts acc " \
+        "ON lo.account_id=acc.id ORDER BY lo.id"
+    result = db.session.execute(sql)
+    loan_history = result.fetchall()
+    return loan_history
 
 def return_loan(account_id, material_id):
     id = find_loan_id(account_id, material_id)
@@ -68,3 +88,9 @@ def return_loan(account_id, material_id):
         return True
     except:
         return False
+
+def times_loaned(material_id):
+    sql = "SELECT COUNT(*) FROM loans WHERE material_id=:material_id"
+    result = db.session.execute(sql, {"material_id":material_id})
+    times_loaned = result.fetchone
+    return times_loaned
